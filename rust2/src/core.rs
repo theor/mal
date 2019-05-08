a3ba3479c704d6e03996b1ae6df80071a7b2b25a
Theo Richart <theor@unity3d.com>
2019-05-08 12:36:37 -0400

    step 4 core ns

---
+++ rust2/src/core.rs
@@ -0,0 +1,82 @@
+use crate::ast::Ast::*;
+use crate::ast::{Ast, MalRes, MalErr, MalArgs};
+
+fn int_op(op: fn(i64, i64) -> Ast, a: &MalArgs) -> MalRes {
+  match (a[0].clone(), a[1].clone()) {
+    (Int(a0), Int(a1)) => Ok(op(a0, a1)),
+    _ => Err(MalErr::ErrString("invalid int_op args".to_owned())),
+  }
+}
+
+fn eq(a: &Ast, b: &Ast) -> MalRes {
+  match (a, b) {
+    (List(ref a), List(ref b)) | (Vector(ref a), Vector(ref b)) => {
+      if a.len() != b.len() {
+        Ok(Bool(false))
+      } else {
+        a.iter()
+          .zip(b.iter())
+          .try_fold(Bool(true), |_prev, (a, b)| eq(a, b))
+      }
+    }
+    (Int(a), Int(b)) => Ok(Bool(a == b)),
+    (Bool(a), Bool(b)) => Ok(Bool(a == b)),
+    (Sym(ref a), Sym(ref b)) => Ok(Bool(a == b)),
+    (MalString(ref a), MalString(ref b)) => Ok(Bool(a == b)),
+    (Nil, Nil) => Ok(Bool(true)),
+    // (Fun, Fun) => Ok(Bool(a == b)),
+    // (MalFun, MalFun) => Ok(Bool(a == b)),
+    _ => Ok(Bool(false)),
+  }
+}
+
+pub fn ns() -> Vec<(&'static str, Ast)> {
+    vec![
+    ("+", Fun(|args, _env| int_op(|i, j| Int(i + j), args))),
+    ("-", Fun(|args, _env| int_op(|i, j| Int(i - j), args))),
+    ("/", Fun(|args, _env| int_op(|i, j| Int(i / j), args))),
+    ("*", Fun(|args, _env| int_op(|i, j| Int(i * j), args))),
+    ("<", Fun(|args, _env| int_op(|i, j| Bool(i < j), args))),
+    (">", Fun(|args, _env| int_op(|i, j| Bool(i > j), args))),
+    ("<=", Fun(|args, _env| int_op(|i, j| Bool(i <= j), args))),
+    (">=", Fun(|args, _env| int_op(|i, j| Bool(i >= j), args))),
+    (
+      "list",
+      Fun(|args, _env| Ok(List(args.iter().cloned().collect()))),
+    ),
+    (
+      "list?",
+      Fun(|args, _env| match args[0] {
+        List(_) => Ok(Bool(true)),
+        _ => Ok(Bool(false)),
+      }),
+    ),
+    (
+      "prn",
+      Fun(|args, _env| {
+        for arg in args.get(0).iter() {
+          println!("{}", &arg);
+        }
+        Ok(Nil)
+      }),
+    ),
+    (
+      "empty?",
+      Fun(|args, _env| match &args[0] {
+        List(ref l) => Ok(Bool(l.len() == 0)),
+        _ => Ok(Bool(false)),
+      }),
+    ),
+    (
+      "count",
+      Fun(|args, _env| match &args[0] {
+        List(ref l) => Ok(Int(l.len() as i64)),
+        _ => Ok(Int(0)),
+      }),
+    ),
+    (
+      "=",
+      Fun(|args, _env| eq(&args[0], &args[1])),
+    ),
+  ]
+}
--- rust2/src/step4_if_fn_do.rs
+++ rust2/src/step4_if_fn_do.rs
@@ -7,6 +7,7 @@ extern crate regex;
 mod ast;
 mod env;
 mod reader;
+mod core;
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
@@ -154,37 +155,6 @@ fn eval(ast: &Ast, env: &mut Env) -> MalRes {
   }
 }
 
-fn int_op(op: fn(i64, i64) -> Ast, a: &MalArgs) -> MalRes {
-  use ast::Ast::Int;
-  match (a[0].clone(), a[1].clone()) {
-    (Int(a0), Int(a1)) => Ok(op(a0, a1)),
-    _ => Err(MalErr::ErrString("invalid int_op args".to_owned())),
-  }
-}
-
-fn eq(a: &Ast, b: &Ast) -> MalRes {
-  use ast::Ast::*;
-  match (a, b) {
-    (List(ref a), List(ref b)) | (Vector(ref a), Vector(ref b)) => {
-      if a.len() != b.len() {
-        Ok(Bool(false))
-      } else {
-        a.iter()
-          .zip(b.iter())
-          .try_fold(Bool(true), |_prev, (a, b)| eq(a, b))
-      }
-    }
-    (Int(a), Int(b)) => Ok(Bool(a == b)),
-    (Bool(a), Bool(b)) => Ok(Bool(a == b)),
-    (Sym(ref a), Sym(ref b)) => Ok(Bool(a == b)),
-    (MalString(ref a), MalString(ref b)) => Ok(Bool(a == b)),
-    (Nil, Nil) => Ok(Bool(true)),
-    // (Fun, Fun) => Ok(Bool(a == b)),
-    // (MalFun, MalFun) => Ok(Bool(a == b)),
-    _ => Ok(Bool(false)),
-  }
-}
-
 fn main() {
   // `()` can be used when no completer is required
 
@@ -193,60 +163,12 @@ fn main() {
     println!("No previous history.");
   }
 
-  use ast::Ast::*;
-  let ns = vec![
-    ("+", Fun(|args, _env| int_op(|i, j| Int(i + j), args))),
-    ("-", Fun(|args, _env| int_op(|i, j| Int(i - j), args))),
-    ("/", Fun(|args, _env| int_op(|i, j| Int(i / j), args))),
-    ("*", Fun(|args, _env| int_op(|i, j| Int(i * j), args))),
-    ("<", Fun(|args, _env| int_op(|i, j| Bool(i < j), args))),
-    (">", Fun(|args, _env| int_op(|i, j| Bool(i > j), args))),
-    ("<=", Fun(|args, _env| int_op(|i, j| Bool(i <= j), args))),
-    (">=", Fun(|args, _env| int_op(|i, j| Bool(i >= j), args))),
-    (
-      "list",
-      Fun(|args, _env| Ok(List(args.iter().cloned().collect()))),
-    ),
-    (
-      "list?",
-      Fun(|args, env| match eval_ast(&args[0], env)? {
-        List(_) => Ok(Bool(true)),
-        _ => Ok(Bool(false)),
-      }),
-    ),
-    (
-      "prn",
-      Fun(|args, _env| {
-        for arg in args.get(0).iter() {
-          println!("{}", &arg);
-        }
-        Ok(Nil)
-      }),
-    ),
-    (
-      "empty?",
-      Fun(|args, env| match eval_ast(&args[0], env)? {
-        List(ref l) => Ok(Bool(l.len() == 0)),
-        _ => Ok(Bool(false)),
-      }),
-    ),
-    (
-      "count",
-      Fun(|args, env| match eval_ast(&args[0], env)? {
-        List(ref l) => Ok(Int(l.len() as i64)),
-        _ => Ok(Int(0)),
-      }),
-    ),
-    (
-      "=",
-      Fun(|args, env| eq(&eval_ast(&args[0], env)?, &eval_ast(&args[1], env)?)),
-    ),
-  ];
-
   let mut env = Env::new();
-  for (n, v) in ns.into_iter() {
+  for (n, v) in core::ns().into_iter() {
     env.insert(&n.into(), v).unwrap();
   }
+
+  reader::read_str(&"(def! not (fn* (a) (if a false true)))".to_owned()).and_then(|x| eval(&x, &mut env)).unwrap();
 
   loop {
     let readline = rl.readline("user> ");
